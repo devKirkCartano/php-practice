66c7bed99dd995014752afbfe3934b2a21aed695
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2023-06-17 18:39:05 +0800

    initial commit

---
+++ arithmetic.php
@@ -0,0 +1,30 @@
+<?php
+  // Arithmetic Operators
+  $x = 10;
+  $y = 3;
+  $z = null;
+
+  // $z = $x + $y;
+  // $z = $x - $y;
+  // $z = $x * $y;
+  // $z = $x / $y;
+  // $z = $x ** $y; // exponentiation
+  $z = $x % $y; // modulus - remainder
+  // echo $z;
+
+  // Increment and Decrement Operators
+
+  $counter = 10;
+  // $counter++; // post-increment
+  // ++$counter; // pre-increment
+  // $counter--; // post-decrement
+  // --$counter; // pre-decrement
+  $counter += 5; // $counter = $counter + 5;
+  echo $counter; 
+
+  // Operator Precedence
+  // ()
+  // **
+  // * / %
+  // + -
+?>
