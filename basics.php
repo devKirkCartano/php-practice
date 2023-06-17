f8e20cd87fc9dd3132d7f2a4dc7c48e27f726fe1
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2023-06-17 16:27:31 +0800

    initial commit

---
+++ basics.php
@@ -0,0 +1,20 @@
+<?php
+  // print text to screen
+  echo "I like pizza <br>";
+  echo "It's really good";
+
+  // This is a comment
+  /*Multiline comment*/ 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Php-Basics</title>
+</head>
+<body>
+  <br>
+  <button>order a pizza</button>
+</body>
+</html>
